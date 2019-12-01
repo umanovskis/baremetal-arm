0e147bb29665ff67e46db397e9b2534fb581e602
Daniels Umanovskis <daniels@umanovskis.se>
2019-12-01 16:20:17 +0100

    Remove some more traces of 09 from 08

--- src/08_scheduler/src/process.h
+++
@@ -1,7 +0,0 @@
-typedef uint32_t* stack_pointer;
-typedef uint32_t cpu_register;
-
-typedef struct {
-    stack_pointer stack_top;
-    cpu_register registers[16]; /* R0 to R15 */
-} process_context;
