4cd7ff95f4cc70b6a368db8e71343468cc9619b4
Daniels Umanovskis <daniels@umanovskis.se>
2019-12-08 19:03:58 +0100

    Rename sched_preemptive -> sched

--- src/09_context_switch/src/sched.c
+++ src/09_context_switch/src/sched.c
@@ -1,9 +1,12 @@
 #include <stddef.h>
-#include <stdint.h>
 #include "sched.h"
 
 static task_desc task_table[MAX_NUM_TASKS] = {0};
 static uint8_t table_idx = 0;
+
+static task_context csa[MAX_NUM_TASKS] = {0};
+//static process_context* current_context;
+static task_desc* current_task;
 
 sched_error sched_add_task(task_entry_ptr entry, systime_t period) {
     if (table_idx >= MAX_NUM_TASKS) {
@@ -11,28 +14,91 @@ sched_error sched_add_task(task_entry_ptr entry, systime_t period) {
     }
 
     task_desc task = {
+        .id = table_idx,
         .entry = entry,
         .period = period,
-        .last_run = 0
+        .last_run = 0,
+        .context = &csa[table_idx]
     };
     task_table[table_idx++] = task;
 
     return SCHED_OK;
 }
 
-void sched_run(void) {
-    while (1) {
-        for (uint8_t i = 0; i < MAX_NUM_TASKS; i++) {
-            task_desc* task = &task_table[i];
-            if (task->entry == NULL) {
-                continue;
-            }
+static void save_context(task_context* ctx) {
+//    asm volatile(
+//    ".arm \t\n"
+//    "stmia r0!, {r0-r14}^ \t\n"
+//    );
+}
 
-            //if (task->last_run + task->period <= systime_get()) { /* Overflow bug! */
-            if (systime_get() - task->last_run >= task->period) {
-                task->last_run = systime_get();
-                task->entry();
-            }
+static int task_switch_callback(void* arg) {
+    uint8_t new_task_id = *((uint8_t*)arg);
+    if (current_task && (new_task_id == current_task->id)) {
+        return 0;
+    }
+    uart_write("Switching context! Time ");
+    uart_write_uint(systime_get());
+    uart_write("; ");
+    if (current_task) {
+        uart_write_uint(current_task->id);
+    } else {
+        uart_write("(idle)");
+    }
+    uart_write(" --> ");
+    uart_write_uint(new_task_id);
+    uart_write("\n");
+    if (current_task) {
+        save_context(&csa[current_task->id]);
+    }
+    current_task = &(task_table[new_task_id]);
+
+    return 0;
+}
+
+static uint32_t saved_regs[12];
+static uint32_t saved_sp;
+
+static void run_task(task_entry_ptr entry) {
+    uint32_t mode;
+    uint32_t* regs = saved_regs;
+    uint32_t cpsr;
+
+    asm("mrs r1, cpsr \n\t"
+        "push {r1, lr} \n\t"
+        "bic r1, r1, 0x3 \n\t"
+        "msr cpsr, r1"
+        );
+
+    entry();
+    asm("svc 0 \n\t"
+    "pop {r0, lr} \n\t"
+    "msr cpsr, r0");
+}
+
+void sched_end_task(uint32_t next) {
+    uint32_t sp_local = saved_sp;
+    asm("mov sp, %0" : : "r"(sp_local));
+    asm("mov pc, r0");
+    return;
+    asm("pop {r1-r7, lr}");
+    asm("mov pc, r0");
+}
+
+void sched_run(void) {
+    for (uint8_t i = 0; i < MAX_NUM_TASKS; i++) {
+       task_desc* task = &task_table[i];
+       if (task->entry == NULL) {
+            continue;
+       }
+       systime_t next_run = task->last_run + task->period;
+       systime_schedule_event(next_run, task->period, task_switch_callback, &task->id);
+    }
+    current_task = &task_table[0]; // Simplification: always start the first task added
+    while(1) {
+        if (current_task) {
+            run_task(current_task->entry);
+            current_task = NULL;
         }
     }
 }
--- src/09_context_switch/src/sched.h
+++ src/09_context_switch/src/sched.h
@@ -1,11 +1,42 @@
+#include <stdint.h>
 #include "systime.h"
+
+typedef uint32_t* stack_pointer;
+typedef uint32_t cpu_register;
+
+/*typedef struct {
+    stack_pointer stack_top;
+    cpu_register registers[16]; /* R0 to R15 */
+/* } task_context;*/
+
+typedef struct {
+    uint32_t r0;
+    uint32_t r1;
+    uint32_t r2;
+    uint32_t r3;
+    uint32_t r4;
+    uint32_t r5;
+    uint32_t r6;
+    uint32_t r7;
+    uint32_t r8;
+    uint32_t r9;
+    uint32_t r10;
+    uint32_t r11;
+    uint32_t r12;
+    uint32_t sp;
+    uint32_t lr;
+    uint32_t pc;
+    uint32_t spsr;
+} task_context;
 
 typedef void (*task_entry_ptr)(void);
 
 typedef struct {
+    uint8_t id;
     task_entry_ptr entry;
     systime_t period;
     systime_t last_run;
+    task_context* context;
 } task_desc;
 
 typedef enum {
@@ -17,3 +48,5 @@ typedef enum {
 
 sched_error sched_add_task(task_entry_ptr entry, systime_t period);
 void sched_run(void);
+
+void sched_end_task(uint32_t);
